4776c612133cec7a03e12db1af9f84462efbd362
Alexander Dubois <sander.dubois@outlook.com>
2018-11-12 13:06:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var playArray = []
+
+  for(var i=0; i<musicians.length; i++) {
+    playArray[i] = musicians[i] + " plays " + instruments[i]
+  }
+
+  return playArray
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  while(i < facts.length) {
+    facts[i] = facts[i] + "!!!"
+    i++
+  }
+  return facts
+}
+
+//or: facts.map(x => x + "!!!")
+
+function iLoveTheBeatles(num) {
+   var arr = [];
+   console.log("A", arr.length)
+   do {
+     arr.push = "I love the Beatles!";
+     console.log("B", arr.length)
+     num++;
+   } while(num < 15)
+
+   console.log("C", arr.length)
+   return arr;
+}
